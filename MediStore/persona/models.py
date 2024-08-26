525d19246effa2db493e43bb68387780ad08b91e
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-08-26 15:55:46 -0500

    Creación del modelo para persona

--- MediStore/persona/models.py
+++ MediStore/persona/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+    TIPOS_DOCUMENTO = [
+        ('CC', 'Cédula de Ciudadanía'),
+        ('TI', 'Tarjeta de Identidad'),
+        ('CE', 'Cédula de Extranjería'),
+        ('PA', 'Pasaporte'),
+    ]
+
+    nombre_completo = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    tipo_documento = models.CharField(max_length=2, choices=TIPOS_DOCUMENTO)
+    numero_documento = models.CharField(max_length=20, primary_key=True)
+    numero_telefono = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre_completo
