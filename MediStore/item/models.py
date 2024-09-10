31be1f5d484f621adbe135b8f85c76d01046ed1c
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-09 23:25:13 -0500

    Creación del modelo Item

--- MediStore/item/models.py
+++ MediStore/item/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from .models import Producto, Lote
 
-# Create your models here.
+class Item(models.Model):
+    ESTADO_OPCIONES = [
+        ('disponible', 'Disponible'),
+        ('vendido', 'Vendido'),
+        ('danado', 'Dañado'),
+        ('vencido', 'Vencido'),
+    ]
+
+    codigo_barras = models.CharField(max_length=50, primary_key=True)
+    producto_id = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    codigo_lote = models.ForeignKey(Lote, on_delete=models.CASCADE, to_field='codigo')
+    estado = models.CharField(max_length=10, choices=ESTADO_OPCIONES, default='disponible')
+
+    def __str__(self):
+        return f'{self.codigo_barras} - {self.producto_id.nombre} - {self.estado}'
