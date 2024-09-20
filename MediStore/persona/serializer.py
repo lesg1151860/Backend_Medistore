5d97883a5f680e860493ea77f0f0c489b5b5d508
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-19 22:51:52 -0500

    Coreeción del fields

--- MediStore/persona/serializer.py
+++ MediStore/persona/serializer.py
@@ -4,4 +4,4 @@ from .models import Persona
 class PersonaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Persona
-        fields = ['nombre_completo' , 'numero_telefono']
+        fields = ['nombre_completo', 'email', 'tipo_documento', 'numero_documento', 'numero_telefono']
