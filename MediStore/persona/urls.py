77cb55ba3c1a50df84456643ba02307837a58e0c
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-19 22:31:17 -0500

    Inclución de la url para serializer del modelo Persona

--- MediStore/persona/urls.py
+++ MediStore/persona/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-from . import views
+from .views import PersonaListCreateView
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path('api/personas/', PersonaListCreateView.as_view(), name='personas-list-create'),
 ]
+
