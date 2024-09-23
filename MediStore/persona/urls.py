beb54804c7b3715e6cd98af945d1df8d3ae7eb96
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-22 23:33:24 -0500

    Agregando configuración de urls para cada petición

--- MediStore/persona/urls.py
+++ MediStore/persona/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-from .views import PersonaListCreateView
+from django.urls import  path
+from .views import PersonaCreateView, PersonaDeleteView, PersonaListView, PersonaSearchView, PersonaUpDateView
 
 urlpatterns = [
-    path('api/personas/', PersonaListCreateView.as_view(), name='personas-list-create'),
+    path('personas/', PersonaListView.as_view(), name='persona-list'),
+    path('personas/create/', PersonaCreateView.as_view(), name='persona-create'),
+    path('personas/search/<str:numero_documento>/', PersonaSearchView.as_view(), name='persona-search'),
+    path('personas/update/<str:nombre_completo>/', PersonaUpDateView.as_view(), name='persona-update'),
+    path('personas/delete/<str:numero_documento>/', PersonaDeleteView.as_view(), name='persona-delete'),
 ]
-
