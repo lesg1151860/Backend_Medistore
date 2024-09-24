efabf4b3f57a8ee7545f4be1660984863fd594c1
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-24 18:50:48 -0500

    Registro de urls para métodos del modelo Usuario

--- MediStore/usuario/urls.py
+++ MediStore/usuario/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UsuarioCreateView, UsuarioListView
+from .views import UsuarioCreateView, UsuarioDeleteView, UsuarioListView, UsuarioUpdateView
 
 urlpatterns = [
-    path('usuarios/', UsuarioListView.as_view(), name='usuer-list'),  # URL para listar los usuarios
-    path('usuarios/create/', UsuarioCreateView.as_view(), name='usuario-create'),  # Para la vista basada en clases
+    path('usuarios/', UsuarioListView.as_view(), name='usuer-list'),
+    path('usuarios/create/', UsuarioCreateView.as_view(), name='usuario-create'),
+    path('usuarios/update/<str:username>/', UsuarioUpdateView.as_view(), name='usuario-update'),
+    path('usuarios/delete/<str:username>/', UsuarioDeleteView.as_view(), name='usuario-delete'),
 ]
