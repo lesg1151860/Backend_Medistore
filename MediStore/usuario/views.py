ace2f9963d1075a19daf8491f9aef22890712385
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-24 18:21:53 -0500

    Creación vistas para manejo del serializer del modelo Usuario

--- MediStore/usuario/views.py
+++ MediStore/usuario/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Usuario
+from .serializer import UsuarioSerializer
 
-# Create your views here.
+# Vista para listar todos los usuarios
+class UsuarioListView(generics.ListAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+# Vista para crear un nuevo usuario
+class UsuarioCreateView(generics.CreateAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
