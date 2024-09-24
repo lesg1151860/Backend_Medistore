db52e5249023fac08b5bf43574af3ef27384bb8a
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-24 18:51:51 -0500

    Creación vistas para manejo de métodos en  modelo Usuario

--- MediStore/usuario/views.py
+++ MediStore/usuario/views.py
@@ -1,13 +1,28 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Usuario
-from .serializer import UsuarioSerializer
+from .serializer import CreateUsuarioSerializer, UsuarioSerializer, UsuarioUpdateSerializer
 
-# Vista para listar todos los usuarios
 class UsuarioListView(generics.ListAPIView):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
 
-# Vista para crear un nuevo usuario
 class UsuarioCreateView(generics.CreateAPIView):
     queryset = Usuario.objects.all()
-    serializer_class = UsuarioSerializer
+    serializer_class = CreateUsuarioSerializer
+    
+class UsuarioUpdateView(generics.UpdateAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioUpdateSerializer
+    lookup_field = 'username'
+    
+class UsuarioDeleteView(generics.DestroyAPIView):
+    queryset = Usuario.objects.all()
+    lookup_field = 'username'
+
+    def destroy(self, request, *args, **kwargs):
+        username = self.kwargs['username']
+        if request.user.username == username:
+            return Response({"error": "No puedes eliminar tu propio usuario."}, status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)
