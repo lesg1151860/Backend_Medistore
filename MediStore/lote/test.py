cd9082701dfe02753ca213942743e87e19019895
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-24 22:49:53 -0500

    Eliminación de archivos de pruebas duplicadas

--- MediStore/lote/test.py
+++ MediStore/lote/test.py
@@ -1,16 +1,3 @@
-from rest_framework import serializers
-from .models import Lote
-from django.utils import timezone
+from django.test import TestCase
 
-class LoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lote
-        fields = ['codigo_barras', 'fecha_ingreso', 'fecha_vencimiento']
-
-    def validate_fecha_vencimiento(self, value):
-        """
-        Verifica que la fecha de vencimiento no sea anterior a la fecha de ingreso.
-        """
-        if value < timezone.now().date():
-            raise serializers.ValidationError("La fecha de vencimiento no puede ser anterior a la fecha actual.")
-        return value
+# Create your tests here.
