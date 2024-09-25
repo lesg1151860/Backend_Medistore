a70497ea0d4b5d1067be6b2eea1e6b5798a4e8ac
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-24 22:49:30 -0500

    Creación archivo para pruebas unitarias

---
+++ MediStore/lote/test.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Lote
+from django.utils import timezone
+
+class LoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lote
+        fields = ['codigo_barras', 'fecha_ingreso', 'fecha_vencimiento']
+
+    def validate_fecha_vencimiento(self, value):
+        """
+        Verifica que la fecha de vencimiento no sea anterior a la fecha de ingreso.
+        """
+        if value < timezone.now().date():
+            raise serializers.ValidationError("La fecha de vencimiento no puede ser anterior a la fecha actual.")
+        return value
