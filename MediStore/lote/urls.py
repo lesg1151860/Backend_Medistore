6c0b3baddd045b9175d45cb40425a1d5658cbe26
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-25 17:10:53 -0500

    Configuración de urls para modelo Lote

--- MediStore/lote/urls.py
+++ MediStore/lote/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import LoteListView
+from .views import LoteCreateView, LoteDeleteView, LoteListView
 
 urlpatterns = [
     path('lotes/', LoteListView.as_view(), name='lote-list'),
+    path('lotes/create/', LoteCreateView.as_view(), name='lote-create'),
+    path('lotes/delete/<str:codigo_barras>/', LoteDeleteView.as_view(), name='eliminar-lote'),
 ]
