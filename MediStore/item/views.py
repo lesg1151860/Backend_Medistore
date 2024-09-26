070033259c60ba41f36638cbc8dd00fda1b48487
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-26 14:30:13 -0500

    Vista para método PUT

--- MediStore/item/views.py
+++ MediStore/item/views.py
@@ -1,7 +1,15 @@
 from rest_framework import generics
 from .models import Item
-from .serializer import ItemSerializer
+from .serializer import ItemEstadoSerializer, ItemSerializer
 
 class ItemListCreateView(generics.ListCreateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+class ItemEstadoUpdateView(generics.UpdateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemEstadoSerializer
+    lookup_field = 'codigo_barras'  # Esto asume que estás usando 'codigo_barras' como identificador
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
