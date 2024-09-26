7c79ef796c97c4482a33497ff1b31867554adfb1
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-26 14:29:27 -0500

    Serializer para método PUT

--- MediStore/item/serializer.py
+++ MediStore/item/serializer.py
@@ -5,3 +5,17 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+        
+    def create(self, validated_data):
+        return Item.objects.create(**validated_data)
+
+class ItemEstadoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['estado']  # Solo se permitirá actualizar el campo estado
+
+    def validate_estado(self, value):
+        # Aquí podrías agregar validaciones personalizadas si es necesario
+        if value not in dict(Item.ESTADO_OPCIONES).keys():
+            raise serializers.ValidationError("El estado seleccionado no es válido.")
+        return value
