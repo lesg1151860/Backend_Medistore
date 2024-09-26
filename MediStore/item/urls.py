7bddaf2173ac0f154cee31e34da5f42d57d1b138
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-26 14:29:51 -0500

    Registro url para método PUT

--- MediStore/item/urls.py
+++ MediStore/item/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ItemListCreateView  
+from .views import ItemEstadoUpdateView, ItemListCreateView  
 
 urlpatterns = [
     path('items/', ItemListCreateView.as_view(), name='item-list'),
+    path('items/update-status/<str:codigo_barras>/', ItemEstadoUpdateView.as_view(), name='item-estado-update'),
 ]
