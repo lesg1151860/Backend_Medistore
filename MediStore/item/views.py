000e8bc55299b480eacdc909e1f680ad4327caea
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-26 13:18:57 -0500

    Creación vista de método GET para modelo Item

--- MediStore/item/views.py
+++ MediStore/item/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Item
+from .serializer import ItemSerializer
 
-# Create your views here.
+class ItemListCreateView(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
