5bc747d8cc31d4e5aa05605de052de34e452ceef
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-27 21:13:34 -0500

    Creación de vistas para el modelo Item

--- MediStore/factura/views.py
+++ MediStore/factura/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Factura
+from .serializer import FacturaSerializer
+from rest_framework import status
 
-# Create your views here.
+class FacturaViewSet(viewsets.ModelViewSet):
+    queryset = Factura.objects.all()
+    serializer_class = FacturaSerializer
+
+class FacturaCreateView(APIView):
+    
+    def post(self, request, format=None):
+        serializer = FacturaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
