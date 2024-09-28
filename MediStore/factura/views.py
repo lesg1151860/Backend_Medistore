9c5cdd201b92d8a89e28b1d6d734a3fd7a3126fa
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-09-27 22:55:44 -0500

    Creación de vistas para el modelo Factura

--- MediStore/factura/views.py
+++ MediStore/factura/views.py
@@ -1,19 +1,43 @@
-from rest_framework import viewsets
-from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView
+from rest_framework import status
 from rest_framework.response import Response
 from .models import Factura
 from .serializer import FacturaSerializer
-from rest_framework import status
 
-class FacturaViewSet(viewsets.ModelViewSet):
+class FacturaListView(ListAPIView):
     queryset = Factura.objects.all()
     serializer_class = FacturaSerializer
 
-class FacturaCreateView(APIView):
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
-    def post(self, request, format=None):
-        serializer = FacturaSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class FacturaCreateView(CreateAPIView):
+    queryset = Factura.objects.all()
+    serializer_class = FacturaSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        return Response(response.data, status=status.HTTP_201_CREATED)
+
+class FacturaDetailView(RetrieveAPIView):
+    queryset = Factura.objects.all()
+    serializer_class = FacturaSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class FacturaDeleteView(DestroyAPIView):
+    queryset = Factura.objects.all()
+    serializer_class = FacturaSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Factura.DoesNotExist:
+            return Response({"error": "Factura no encontrada."}, status=status.HTTP_404_NOT_FOUND)
