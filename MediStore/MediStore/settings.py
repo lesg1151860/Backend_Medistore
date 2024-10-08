a79d3113378e4362650571764af24475c9074a82
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-10-08 11:10:15 -0500

    Configuración para desplegar en Vercel

--- MediStore/MediStore/settings.py
+++ MediStore/MediStore/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -60,10 +61,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'MediStore.urls'
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Asegúrate de que esta línea esté correctamente configurada
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
