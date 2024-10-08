3d74802cbecb7bc5d184c0233203f49ec870b071
lesg1151860 <luiseduardosg@ufps.edu.co>
2024-10-08 11:26:06 -0500

    Configuración para despliegue

--- MediStore/MediStore/wsgi.py
+++ MediStore/MediStore/wsgi.py
@@ -8,11 +8,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
+# Establece el módulo de configuración de Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MediStore.settings')
 
+# Obtiene la aplicación WSGI
 application = get_wsgi_application()
+application = WhiteNoise(application)
 
 app = application
